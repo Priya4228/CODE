7821dc353228b86b5459b67c9287a3cc54c40e4a
GliciniaD <190259001+GliciniaD@users.noreply.github.com>
2026-03-23 21:20:44 +0000

    add  route so I can test webhook.py

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello from Flask!If you can see this, the code has been deployed to your webapp. Test to see if all pipelines run again 2x"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Webhook received!")
+    print(data)
+    return "", 200
